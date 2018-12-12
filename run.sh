9cd2636e73c7c24393135569f85cfab7b0fd7f3a
Artem Yankov <yankovai3@gmail.com>
2018-12-12 15:20:44 -0800

    test parallel

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+export SHERPA_DB_HOST==$(echo $PS_HOSTS | awk -F ':' '{print $1}')
+export SHERPA_DB_PORT=5000
 
-apt-get update
+if [ "$TYPE" = "ps" ]; then
+    # install mongo and open up port
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+    echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+    apt-get update
+    apt-get install -y mongodb-org
+    mongod --port 5000
+else
+    python examples/parallel-examples/bianchini/runner.py -l $WORKER_HOSTS
+fi
 
-apt-get install -y mongodb-org
-mongod --port 5000 &
+
+
+
+
