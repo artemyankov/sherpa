7cb84506aa679a3e6021c238c64543f5a8e1c989
Artem Yankov <yankovai3@gmail.com>
2018-12-12 14:04:29 -0800

    new commands for mongo

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
 
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
-echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-apt-get update
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
+#echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+#apt-get update
+apt-get install -y libcurl3 openssl
+
+
+
 apt-get install -y mongodb-org
 mongod --port 5000 &
