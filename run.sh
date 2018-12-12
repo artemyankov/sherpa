f27eb48a5894088465eafbced3f22b2e058d4207
Artem Yankov <yankovai3@gmail.com>
2018-12-12 12:16:10 -0800

    changing install instructs

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-apt install -y mongodb
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
+echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+apt-get update
+apt-get install -y mongodb-org
 mongod --port 5000 &
-echo "[+] Mongo started..."
