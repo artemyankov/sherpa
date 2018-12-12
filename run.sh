eec7c0cb6b7e5574eb1593254ce2db7308af16fb
Artem Yankov <yankovai3@gmail.com>
2018-12-12 15:36:51 -0800

    adding sherpa

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 export SHERPA_DB_HOST==$(echo $PS_HOSTS | awk -F ':' '{print $1}')
 export SHERPA_DB_PORT=5000
 
-if [ "$TYPE" = "ps" ]; then
+if [$TYPE = "ps" ]; then
     # install mongo and open up port
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
     echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
